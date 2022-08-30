e6bc49b80936b1308ff844c62d107db82a235608
basthub <89909922+basthub@users.noreply.github.com>
2022-08-30 16:21:48 +0200

    fixed Bills sort

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -24,7 +24,7 @@ const rows = (data) => {
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+  bills = bills?.sort((a, b) => ((a.date < b.date) ? 1 : -1)) 
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
